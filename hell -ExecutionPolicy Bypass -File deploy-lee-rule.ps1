a048d7e09ae7a721ce42a88532ffb8340cb55f2c
leewaller93 <leewaller93@gmail.com>
2025-08-02 10:33:25 -0500

    Update backend and demo data for public demo

---
+++ hell -ExecutionPolicy Bypass -File deploy-lee-rule.ps1
@@ -0,0 +1,5 @@
+[33m5d1a85a[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m Update backend and demo data for public demo
+[33m968f65e[m Update backend and demo data for public demo
+[33m7a89cca[m Update backend and demo data for public demo
+[33m4a4c132[m Update backend and demo data for public demo
+[33m0348695[m Update backend and demo data for public demo
